a4089554fa5b1b121a4c71acdbb7d7b7111b43c2
Juan Villarroel <=>
2019-07-27 15:40:13 -0400

    Edit access-control-allow-origin

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(express.json());
 
 // Access
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
     res.setHeader('Access-Control-Allow-Credentials', true);
